0b338eeca6eb7059fd6578d6da4baa47d6ead173
Vlad <vlad@example.com>
2021-11-26 23:30:27 +0300

    barebones for entity spawn

--- src/game.ts
+++ src/game.ts
@@ -1,7 +1,3 @@
-import * as utils from '@dcl/ecs-scene-utils'
-import { convertMessageToObject } from '@decentraland/EthereumController'
-
-
 function spawnEntity(shape: Shape, x: number, y: number, z: number, rotation?: Quaternion) {
   const entity = new Entity()
   entity.addComponent(new Transform({ position: new Vector3(x, y, z), rotation }))
@@ -10,19 +6,4 @@ function spawnEntity(shape: Shape, x: number, y: number, z: number, rotation?: Q
   return entity
 }
 
-const sceneMessageBus = new MessageBus()
 const moveBoxEntity = spawnEntity(new BoxShape, 3, 1, 3)
-
-moveBoxEntity.addComponent(
-  new OnPointerDown(() => {
-    sceneMessageBus.emit("moveBox", {})
-  })
-)
-
-const moveBox = () => {
-  moveBoxEntity.getComponent(Transform).position.x += 1
-}
-
-//  This one being triggered, but update to multiplayer scene happence only once
-sceneMessageBus.on("moveBox", moveBox)
-
