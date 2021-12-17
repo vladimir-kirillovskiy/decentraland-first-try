c795469fe3a3072a1d4307c82ebf67b29c2eb0ab
Vlad <vladimir.kirillovskiy@gmail.com>
2021-12-18 02:47:36 +0300

    added a new test scene

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,13 @@
-function spawnEntity(shape: Shape, x: number, y: number, z: number, rotation?: Quaternion) {
+
+function spawnEntity(shape: Shape, position: Vector3, rotation?: Quaternion) {
   const entity = new Entity()
-  entity.addComponent(new Transform({ position: new Vector3(x, y, z), rotation }))
+  
+  entity.addComponent(new Transform({ position: position, rotation }))
   entity.addComponent(shape)
   engine.addEntity(entity)
+  
   return entity
 }
 
-const moveBoxEntity = spawnEntity(new GLTFShape("models/coin.glb"), 3, 1, 3)
+const arc = spawnEntity(new GLTFShape("models/Arc_big.glb"), new Vector3(32, 0, 16)) 
+const scene = spawnEntity(new GLTFShape("models/Scene.glb"), new Vector3(8, -0.5, 16)) 
